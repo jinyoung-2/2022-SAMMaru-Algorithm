50189ff3fa934304013ad4cc4cfed2749421164d
brenda <brenda524@naver.com>
2022-05-26 20:48:46 +0900

    이전에 다른 문제 풀이 업로드 - 2577 숫자의 개수

--- B2/2577-jinyoung-2.java
+++ B2/2577-jinyoung-2.java
@@ -1,42 +1,28 @@
-// BufferedReader 입력 방식 + stream 풀이 방식
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class BOJ_10818 {
+public class BOJ_2577 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());   // 정수의 개수 입력
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
 
-        // String으로 입력받은 것을 split method를 사용하고 정렬 method를 사용하여 int 배열의 값으로 저장함
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+        int result = A * B * C;
+        int[] cntArr = new int[10];
 
-        int min = arr[0];
-        int max = arr[N - 1];
+        while ((result / 10) != 0) {
+            int x = result % 10;
+            cntArr[x]++;
 
-        System.out.printf("%d %d", min, max);
+            result /= 10;
+        }
+        cntArr[result]++;
+
+        // A × B × C의 결과에 0~9가 각각 몇 번 쓰였는지 출력
+        for (int i = 0; i < 10; i++) {
+            System.out.println(cntArr[i]);
+        }
     }
 }
-
-//// Scanner 풀이 방식
-//import java.util.Arrays;
-//import java.util.Scanner;
-//
-//public class BOJ_10818 {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int N = sc.nextInt();   // 정수의 개수 입력
-//
-//        int[] arr = new int[N];
-//        for (int i = 0; i < N; i++) {
-//            arr[i] = sc.nextInt();    // N개 만큼 숫자 입력받기
-//        }
-//
-//        Arrays.sort(arr); // 오름차순 정렬
-//        int min = arr[0];
-//        int max = arr[N - 1];
-//
-//        System.out.printf("%d %d", min, max);
-//    }
-//}
