94e3b3fc7e073974b7eb9ec70c7daaeba2ec96ef
brenda <brenda524@naver.com>
2022-07-04 00:38:05 +0900

    [이진영] 6주차 알고리즘 문제 해결 (#17)

--- S3/1002-jinyoung-2.java
+++ S3/1002-jinyoung-2.java
@@ -19,38 +19,25 @@ public class Main {
             int y2 = Integer.parseInt(st.nextToken());
             int r2 = Integer.parseInt(st.nextToken());
 
-            int powD = (int) (Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
-            int powRadiusMinus = (int) Math.pow(r2 - r1, 2);
-            int powRadiusPlus = (int) Math.pow(r2 + r1, 2);
+            double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
             // Case 1 : 중점 및 반지름 모두 동일한 경우 => 접점의 개수 무한개
-            if (x1 == x2 && y1 == y2 && r1 == r2) {
-                sb.append(-1).append("\n");
+            if (d == 0 && r1 == r2) {
+                sb.append("-1\n");
             }
-
-            // Case 2-1 : 접점이 0개인 경우 => 한 원이 다른 원의 외부에 있는 경우 (단, 접하는 경우 제외)
-            else if (powD > powRadiusPlus) {
-                sb.append(0).append("\n");
+            // Case 2 : 접점이 2개인 경우
+            else if (d > Math.abs(r2 - r1) && d < r1 + r2) {
+                sb.append("2\n");
             }
-
-            // Case 2-2 : 접점이 0개인 경우 => 한 원이 다른 원의 내부에 있는 경우 (단, 접하는 경우 제외)
-            else if (powD < powRadiusMinus) {
-                sb.append(0).append("\n");
-            }
-
             // Case 3-1 : 접점이 1개인 경우 => 내접한 경우
-            else if (powD == powRadiusMinus) {
-                sb.append(1).append("\n");
-            }
-
             // Case 3-2 : 접점이 1개인 경우 => 외접한 경우
-            else if (powD == powRadiusPlus) {
-                sb.append(1).append("\n");
+            else if (Math.abs(r2 - r1) == d || r2 + r1 == d) {
+                sb.append("1\n");
             }
-
-            // Case 4 : 접점이 2개인 경우
-            else {
-                sb.append(2).append("\n");
+            // Case 4-1 : 접점이 0개인 경우 => 한 원이 다른 원의 외부에 있는 경우 (단, 접하는 경우 제외)
+            // Case 4-2 : 접점이 0개인 경우 => 한 원이 다른 원의 내부에 있는 경우 (단, 접하는 경우 제외)
+            else if (r1 + r2 < d || Math.abs(r2 - r1) > d) {
+                sb.append("0\n");
             }
         }
         System.out.print(sb);
